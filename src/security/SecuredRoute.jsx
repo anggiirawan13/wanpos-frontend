caf9be897bd3bcabee9df9176a6302321d764a2f
anggiirawan13 <irawananggi76@gmail.com>
2023-07-21 22:21:35 +0700

    fix secure route

--- src/security/SecuredRoute.jsx
+++ src/security/SecuredRoute.jsx
@@ -1,10 +1,14 @@
 import Storage from "../Storage/storage";
 import jwtDecode from "jwt-decode";
+import Login from "../pages/login/index.jsx";
 
 const SecuredRoute = ({ element: Element, role: Role }) => {
   const auth = Storage.getLogin() ? jwtDecode(Storage.getLogin()) : false;
 
-  if (auth.role == Role || Role == "") {
+  if (!auth) {
+    history.replaceState(null, null, "/login");
+    return <Login />;
+  } else if (auth.role == Role || Role == "") {
     return Element;
   } else {
     return history.back();
